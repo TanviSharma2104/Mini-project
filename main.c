3b63419115713a0b6abbaf7fe21665642ee9a99d
TanviSharma2104 <tanvisharma2703@gmail.com>
2023-09-03 15:18:55 +0530

    updated source code

--- main.c
+++ main.c
@@ -160,6 +160,42 @@ void dischargePatient(){
 }
 
 
+void addDoctor(){
+
+    char myDate[12];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(myDate, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year + 1900);
+    strcpy(d.date, myDate);
+
+    int f=0;
+
+    system("cls");
+    printf("<== Add Doctor ==>\n\n");
+
+    fp = fopen("doctor.txt", "ab");
+
+    printf("Enter Doctor id: ");
+    scanf("%d", &d.id);
+
+    printf("Enter Doctor Name: ");
+    fflush(stdin);
+    gets(d.name);
+
+    printf("Enter Doctor Address: ");
+    fflush(stdin);
+    gets(d.address);
+
+    printf("Doctor Specialize in: ");
+    fflush(stdin);
+    gets(d.specialize);
+
+    printf("Doctor Added Successfully\n\n");
+
+    fwrite(&d, sizeof(d), 1, fp);
+    fclose(fp);
+}
+
 void doctorList(){
     system("cls");
     printf("<== Doctor List ==>\n\n");
